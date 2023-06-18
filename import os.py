b39def183094f10cabcbf720ca658e8f57dda009
parakazanacam <129620303+parakazanacam@users.noreply.github.com>
2023-06-18 21:25:09 +0200

    ddd

--- import os.py
+++ import os.py
@@ -4,7 +4,7 @@ def generate_sitemap(html_files, sitemap_name):
     sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
     sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
     for i, file in enumerate(html_files):
-        sitemap += f'\t<url>\n\t\t<loc>http://cem.com/{file}</loc>\n\t</url>\n'
+        sitemap += f'\t<url>\n\t\t<loc>https://avseetv2023.netlify.app/{file}</loc>\n\t</url>\n'
         if (i + 1) % 300 == 0 or (i + 1) == len(html_files):
             sitemap += '</urlset>'
             with open(f'{sitemap_name}_{i // 14000 + 1}.xml', 'w') as f:
