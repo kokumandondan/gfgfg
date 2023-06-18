219d22547d8729adb88be5f21242170b945b14c7
parakazanacam <129620303+parakazanacam@users.noreply.github.com>
2023-06-18 20:43:06 +0200

    44

---
+++ from urllib.py
@@ -0,0 +1,42 @@
+
+import csv
+
+kodcuk = open("kod.txt", 'r', encoding="utf8")    # Satır verilerini al
+kod = (kodcuk.read())
+
+def create_html_file(row):
+    # HTML şablonunu oluştur
+    template = """
+<html>
+<head>
+{kod}
+<title>{title}</title>
+</script>
+</head>
+<body>
+<h1>{heading}</h1>
+<p>{content}</p>
+</body>
+</html>
+    """
+
+
+    title = row[0]  # İlk sütun
+    heading = row[1]  # İkinci sütun
+    content = row[2]  # Üçüncü sütun
+
+
+    # HTML dosyasını oluştur
+    html_content = template.format(kod=kod, title=title, heading=heading, content=content)
+
+    # HTML dosyasını kaydet
+    filename = f"{title}.html"
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(html_content)
+
+# CSV dosyasını aç ve her satır için HTML dosyası oluştur
+with open('data.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # İlk satırı atla (başlık satırı)
+    for row in reader:
+        create_html_file(row)
---
+++ import os.py
@@ -0,0 +1,20 @@
+import os
+
+def generate_sitemap(html_files, sitemap_name):
+    sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    for i, file in enumerate(html_files):
+        sitemap += f'\t<url>\n\t\t<loc>http://cem.com/{file}</loc>\n\t</url>\n'
+        if (i + 1) % 300 == 0 or (i + 1) == len(html_files):
+            sitemap += '</urlset>'
+            with open(f'{sitemap_name}_{i // 300 + 1}.xml', 'w') as f:
+                f.write(sitemap)
+            sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
+            sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
+# Klasördeki HTML dosyalarını listeleme
+folder_path = 'C:\\Users\\cem\\Documents\\GitHub\\kokuman'  # Klasör yolunu buraya girin
+html_files = [file for file in os.listdir(folder_path) if file.endswith('.html')]
+
+# Sitemap dosyası oluşturma
+generate_sitemap(html_files, 'cem_sitemap')
