f620cceb7a6f2e89f2a17a7ea853c2409ccdeff9
parakazanacam <129620303+parakazanacam@users.noreply.github.com>
2023-06-18 21:24:30 +0200

    jjj

--- import os.py
+++ import os.py
@@ -7,13 +7,13 @@ def generate_sitemap(html_files, sitemap_name):
         sitemap += f'\t<url>\n\t\t<loc>http://cem.com/{file}</loc>\n\t</url>\n'
         if (i + 1) % 300 == 0 or (i + 1) == len(html_files):
             sitemap += '</urlset>'
-            with open(f'{sitemap_name}_{i // 300 + 1}.xml', 'w') as f:
+            with open(f'{sitemap_name}_{i // 14000 + 1}.xml', 'w') as f:
                 f.write(sitemap)
             sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
             sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
 
 # Klasördeki HTML dosyalarını listeleme
-folder_path = 'C:\\Users\\cem\\Documents\\GitHub\\kokuman'  # Klasör yolunu buraya girin
+folder_path = 'C:\\Users\\cem\\Documents\\GitHub\\gfgfg'  # Klasör yolunu buraya girin
 html_files = [file for file in os.listdir(folder_path) if file.endswith('.html')]
 
 # Sitemap dosyası oluşturma
